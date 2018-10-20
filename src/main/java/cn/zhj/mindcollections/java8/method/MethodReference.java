53d51e0865d0062c4f70c2d6b0aae8748633fbef
zhaohj0315.163.com <zhaohj0315.163.com>
2018-10-20 22:53:30 +0800

    te

--- src/main/java/cn/zhj/mindcollections/java8/method/MethodReference.java
+++ src/main/java/cn/zhj/mindcollections/java8/method/MethodReference.java
@@ -34,16 +34,9 @@ public class MethodReference {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
 
         public int getAge() {
             return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
         }
 
         @Override
