73891d84215a9b3ed1936e978afed8ea0cfc9bea
zhaohj0315.163.com <zhaohj0315.163.com>
2018-12-26 15:54:12 +0800

    反转任意个数元素的链表

--- src/main/java/cn/zhj/mindcollections/leecode/list/ReverseNodesInKGroup.java
+++ src/main/java/cn/zhj/mindcollections/leecode/list/ReverseNodesInKGroup.java
@@ -7,7 +7,64 @@ public class ReverseNodesInKGroup {
 
     public ListNode reverseKGroup(ListNode head, int k) {
 
+        if (head == null || k == 1) {
+            return head;
+        }
 
-        return null;
+        ListNode dummy = new ListNode(-1);
+        ListNode pre = dummy, cur = head;
+        dummy.next = head;
+
+        int i = 0;
+        while (cur != null) {
+            ++i;
+            if (i % k == 0) {
+                pre = reverseOneGroup(pre, cur.next);
+                cur = pre.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+
+        return dummy.next;
+    }
+
+    /**
+     * pre指针永远指向局部翻转链表的头节点
+     *
+     * @param pre
+     * @param next
+     * @return
+     */
+    private ListNode reverseOneGroup(ListNode pre, ListNode next) {
+        ListNode last = pre.next;
+        ListNode cur = last.next;
+
+        while (cur != next) {
+            last.next = cur.next;
+            cur.next = pre.next;
+            pre.next = cur;
+            cur = last.next;
+        }
+
+        return last;
+    }
+
+    public static void main(String[] args) {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+        ListNode four = new ListNode(4);
+        ListNode five = new ListNode(5);
+
+        one.next = two;
+        two.next = three;
+        three.next = four;
+        four.next = five;
+
+        ReverseNodesInKGroup reverse = new ReverseNodesInKGroup();
+        ListNode node = reverse.reverseKGroup(one, 2);
+        System.out.println(node.val);
     }
 }
