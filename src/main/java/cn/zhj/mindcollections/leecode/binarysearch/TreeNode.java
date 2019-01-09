d6ecadedc69bd4380f1735eac5098d3bc6acb8c3
zhaohj0315.163.com <zhaohj0315.163.com>
2019-01-09 14:48:06 +0800

    二叉树校验

---
+++ src/main/java/cn/zhj/mindcollections/leecode/binarysearch/TreeNode.java
@@ -0,0 +1,13 @@
+package cn.zhj.mindcollections.leecode.binarysearch;
+
+/**
+ * Created by zhaohongjie on 2019/1/8.
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) { val = x; }
+}
