33ec46c25284d33101d627be94bcbc347046085a
zhaohj0315.163.com <zhaohj0315.163.com>
2019-03-30 16:55:37 +0800

    字符串旋转

---
+++ src/main/java/cn/zhj/mindcollections/interview/string/StringRotation.java
@@ -0,0 +1,23 @@
+package cn.zhj.mindcollections.interview.string;
+
+/**
+ * Created by zhaohongjie on 2019/3/30.
+ */
+public class StringRotation {
+
+    public boolean isRotation(String A, String B) {
+
+        if (A == null || B == null || A.length() != B.length()) {
+            return false;
+        }
+
+        String b2 = B + B;
+
+        return b2.indexOf(A) != -1;
+    }
+
+    public static void main(String[] args) {
+        StringRotation obj = new StringRotation();
+        System.out.println(obj.isRotation("abcd", "cdab"));
+    }
+}
