c8f723c17342134f9a359451494a607a36431703
zhaohj0315.163.com <zhaohj0315.163.com>
2019-04-16 22:48:31 +0800

    树的遍历

---
+++ src/main/java/cn/zhj/mindcollections/interview/binarytree/Node.java
@@ -0,0 +1,17 @@
+package cn.zhj.mindcollections.interview.binarytree;
+
+/**
+ * 二叉树节点
+ *
+ * Created by zhaohongjie on 2019/4/14.
+ */
+public class Node {
+
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
