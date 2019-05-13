1b5d66d033e8ee2a0b27441ddc639ae556acd5d2
zhaohj0315.163.com <zhaohj0315.163.com>
2019-05-13 22:57:21 +0800

    每日一练_打印链表的公共部分

---
+++ src/main/java/alg/zhj/mindcollections/interview/list/Node.java
@@ -0,0 +1,14 @@
+package alg.zhj.mindcollections.interview.list;
+
+/**
+ * Created by zhaohongjie on 2019/5/13.
+ */
+public class Node {
+
+    public int value;
+    public Node next;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
