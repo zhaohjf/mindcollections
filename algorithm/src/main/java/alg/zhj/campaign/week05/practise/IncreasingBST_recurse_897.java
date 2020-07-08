a87b292044ac39631278dc02a86fc4bd72f29762
zhaohongjie03 <zhaohj0315.163.com>
2020-07-09 00:47:06 +0800

    fix

---
+++ algorithm/src/main/java/alg/zhj/campaign/week05/practise/IncreasingBST_recurse_897.java
@@ -0,0 +1,38 @@
+package alg.zhj.campaign.week05.practise;
+
+import alg.zhj.mindcollections.leecode.binarytree.search.TreeNode;
+
+/**
+ * Created by zhaohongjie on 2020/7/9.
+ */
+public class IncreasingBST_recurse_897 {
+
+    TreeNode cur;
+    public TreeNode increasingBST(TreeNode root) {
+        TreeNode ans = new TreeNode(0);
+        cur = ans;
+        inorder(root);
+        return ans.right;
+    }
+
+    public void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        node.left = null;
+        cur.right = node;
+        cur = node;
+        inorder(node.right);
+    }
+
+    public TreeNode _increasingBST(TreeNode root) {
+        return increasingBST(root, null);
+    }
+
+    public TreeNode increasingBST(TreeNode root, TreeNode tail) {
+        if (root == null) return tail;
+        TreeNode res = increasingBST(root.left, root);
+        root.left = null;
+        root.right = increasingBST(root.right, tail);
+        return res;
+    }
+}
