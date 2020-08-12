36d13b36e113e685ed3716fd44709742536ccf93
zhaohongjie03 <zhaohj0315.163.com>
2020-08-12 23:38:33 +0800

    list

---
+++ algorithm/src/main/java/alg/zhj/subject/string/ValidPalindrome_680.java
@@ -0,0 +1,45 @@
+package alg.zhj.subject.string;
+
+/**
+ * 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "aba"
+ * 输出: True
+ * 示例 2:
+ * <p>
+ * 输入: "abca"
+ * 输出: True
+ * 解释: 你可以删除c字符。
+ * 注意:
+ * <p>
+ * 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
+ * <p>
+ * <p>
+ * Created by zhaohongjie on 2020/8/12.
+ */
+public class ValidPalindrome_680 {
+
+    public boolean validPalindrome(String s) {
+        int i = -1, j = s.length();
+        while (++i < --j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
+            }
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int i, int j) {
+        for (int k = i; k <= i + (j - i) / 2; k++) {
+            if (s.charAt(k) != s.charAt(j - k + i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidPalindrome_680 obj = new ValidPalindrome_680();
+        System.out.print(obj.validPalindrome("abc"));
+    }
+}
