58afd2cbe556b619e9490dbf497f3b9214c5e58d
zhaohjf <zhaohj0315@163.com>
2022-02-25 15:10:37 +0800

    Update Trap_42.java
    接雨水 动态规则 + 双指针

--- algorithm/src/main/java/alg/zhj/campaign/week01/Trap_42.java
+++ algorithm/src/main/java/alg/zhj/campaign/week01/Trap_42.java
@@ -27,6 +27,51 @@ public class Trap_42 {
 
         return ans;
     }
+    
+    public int trap_dyn(int[] height) {
+
+        int n = height.length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i > 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return ans;
+    }
+
+    public int trap_dup(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
 
     public static void main(String[] args) {
         int[] a = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
