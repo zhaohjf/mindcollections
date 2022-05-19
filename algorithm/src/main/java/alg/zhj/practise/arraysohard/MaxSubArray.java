e82a6e954a02d3ad19536e7f73c97d51415a8206
hongjie zhao <zhaohj0315@163.com>
2022-05-19 21:17:14 +0800

    最大子数组之和

---
+++ algorithm/src/main/java/alg/zhj/practise/arraysohard/MaxSubArray.java
@@ -0,0 +1,33 @@
+package alg.zhj.practise.arraysohard;
+
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+
+        int ans = dp[0];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+    /**
+     * 状态压缩
+     * @param nums
+     * @return
+     */
+    public int maxSubArray_2(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
